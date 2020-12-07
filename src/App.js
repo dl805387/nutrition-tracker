10dad2af285d57b55e823e5bc9f06a4b64d0c779
dl805387 <59711497+dl805387@users.noreply.github.com>
2020-12-07 00:24:13 -0500

    fetch api

--- src/App.js
+++ src/App.js
@@ -1,15 +1,47 @@
 import './App.css';
-import Form from './Components/Form';
+import React, { useState } from "react";
+//import Form from './Components/Form';
+// 1%20100grams%20apple
 
 function App() {
+  var APP_ID = "cb858920";
+  var APP_KEY = "23e79631876defaed65d9e35dd579c67";
+  const [input, setInput] = useState("");
+  var items = [];
+
+  async function greetings(e) {
+    e.preventDefault(); // prevents page from reloading  
+    var url = "https://api.edamam.com/api/nutrition-data?app_id=" + APP_ID + "&app_key=" + APP_KEY + "&ingr=" + input;
+    items = await fetch(url).then(res => res.json()); // await allows the fetch to complete before moving on to rest of code
+    console.log(items.cautions);
+    console.log(items.totalWeight);
+    console.log(items.ingredients.parsed.nutrients.VITD.label);//.FAT.quantity
+  }
+
+
   return (
     <div className="App">
       <header>
         <h1>Nutrition Tracker</h1>
       </header>
-      <Form />
+
+      <form className="search-form">
+        <input type="text" className="food-input"  onChange={event => setInput(event.target.value)} />
+        <button className="food-button" type="submit"  onClick={greetings} >
+          <i class="fas fa-plus-square"></i>
+        </button>
+        <div className="select">
+          <select name="foods" className="filter-food">
+            <option value="all">All</option>
+            <option value="completed">Completed</option>
+            <option value="uncompleted">Uncompleted</option>
+          </select>
+        </div>
+      </form>
+
     </div>
   );
+
 }
 
 export default App;
