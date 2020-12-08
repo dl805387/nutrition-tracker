8887dfd2af2a967b797a95ae32d0753df42177c7
dl805387 <59711497+dl805387@users.noreply.github.com>
2020-12-07 22:27:10 -0500

    change input value when submit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
 import './App.css';
 import React, { useState } from "react";
 //import Form from './Components/Form';
-// 1%20100grams%20apple
+// 1%20100grams%20apple       // git add -A
 
 function App() {
   var APP_ID = "cb858920";
   var APP_KEY = "23e79631876defaed65d9e35dd579c67";
-  const [input, setInput] = useState("");
+  const [input, setInput] = useState("");             // update code to include the %20, quantity, and size
   var items = [];
 
-  async function addFood(e) {
+  async function searchFood(e) {
     e.preventDefault(); // prevents page from reloading  
     var url = "https://api.edamam.com/api/nutrition-data?app_id=" + APP_ID + "&app_key=" + APP_KEY + "&ingr=" + input;
     items = await fetch(url).then(res => res.json()); // await allows the fetch to complete before moving on to rest of code
-    console.log(items.totalNutrients.ENERC_KCAL.quantity);
-    console.log(items);
+    //console.log(items.totalNutrients.ENERC_KCAL.quantity);
+    //console.log(items);
+   
+    setInput("");
   }
-
 
   return (
     <div className="App">
@@ -24,13 +25,13 @@ function App() {
         <h1>Nutrition Tracker</h1>
       </header>
 
-      <form className="search-form">
-        <input type="text" className="food-input"  onChange={event => setInput(event.target.value)} />
-        <button className="food-button" type="submit"  onClick={addFood} >
-          <i class="fas fa-plus-square"></i>
+      <form className="search-form" onSubmit={searchFood}>
+        <input type="text" className="food-input"  onChange={event => setInput(event.target.value)}  value={input} />
+        <button className="food-button" type="submit">
+          <i className="fas fa-plus-square"></i>
         </button>
         <div className="select">
-          <select name="foods" className="filter-food">
+          <select className="filter-food">
             <option value="all">All</option>
             <option value="completed">Completed</option>
             <option value="uncompleted">Uncompleted</option>
